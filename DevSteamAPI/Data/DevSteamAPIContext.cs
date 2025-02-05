ddc8ae8f9b984c7546aa036d095c8b8969a04c49
Rattini <rattinicursods@gmail.com>
2025-02-05 16:06:20 -0300

    Adicionado os controllers

--- DevSteamAPI/Data/DevSteamAPIContext.cs
+++ DevSteamAPI/Data/DevSteamAPIContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using DevSteamAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DevSteamAPI.Data
@@ -10,10 +11,18 @@ namespace DevSteamAPI.Data
         {
         }
 
+        //Definir as tabelas do banco de dados
+        public DbSet<Jogo> Jogos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
         //Sobrescrever o metodo OnModelCrating
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Jogo>().ToTable("Jogos");
+            modelBuilder.Entity<Categoria>().ToTable("Categorias");
+
+
         }
 
     }
